bbce97068f02a4e25ac44e686cbe8406d13ad765
Joel Weiser <joel.weiser@oicr.on.ca>
2025-10-21 14:51:23 -0400

    merges Runner into Main class

--- src/main/java/org/reactome/orthoinference/Main.java
+++ src/main/java/org/reactome/orthoinference/Main.java
@@ -1,11 +1,24 @@
 package org.reactome.orthoinference;
 
+import org.reactome.orthoinference.eventsInferrer.EventsInferrer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Main {
+public class Main implements CommandLineRunner {
+	private EventsInferrer eventsInferrer;
+
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Main.class, args);
+	}
+
+	public Main(EventsInferrer eventsInferrer) {
+		this.eventsInferrer = eventsInferrer;
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		this.eventsInferrer.inferEvents();
 	}
 }
